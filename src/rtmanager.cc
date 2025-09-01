60812ba8d771433989a1e8f223261fe2b5a2c834
Arjo Chakravarty <arjo@openrobotics.org>
2025-09-01 21:07:36 +0800

    Style
    Signed-off-by: Arjo Chakravarty <arjo@openrobotics.org>

--- src/rtmanager.cc
+++ src/rtmanager.cc
@@ -128,7 +128,8 @@ void RTManager::RenderLoop()
 
       auto frame = msg.mutable_header()->add_data();
       frame->set_key("frame_id");
-      frame->add_value(job.parentFrameId);      
+      frame->add_value(job.parentFrameId);
+      
       pubIt->second.Publish(msg);
     }
     else if (job.sensor->Type() == rtsensor::RtSensor::SensorType::LIDAR)
